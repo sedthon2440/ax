f255988b79cdffe940e5120eaa05e70f2e826d0a
sedthon2440 <166177796+sedthon2440@users.noreply.github.com>
2024-10-10 19:15:48 +0300

    Update YtVideo.py

--- ZelzalMusic/plugins/play/YtVideo.py
+++ ZelzalMusic/plugins/play/YtVideo.py
@@ -11,7 +11,7 @@ from pyrogram.types import Message
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
 
-from AarohiX import app
+from ZelzalMusic import app
 
 
 def get_file_extension_from_url(url):
